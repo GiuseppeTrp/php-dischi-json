e552c5afb177c0c5862583d69209223d806b2253
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-03-20 17:19:34 +0100

    added card

--- index.php
+++ index.php
@@ -6,32 +6,43 @@
     <title>Album Boolify</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        
+        .card {
+	max-width: 190px;
+}
+       
+        
+       
+    </style>
 </head>
 <body>
     
     <div id="app">
-        <div class="container-fluid py-5 ">
-      
-            <h1>Popular albums</h1>
-            <ul class=" container d-flex bg-dark">
-                <li v-for="album in albumList" :key="album.title" class="album-item">
-                    <img :src="album.poster" :alt="album.title" style="max-width: 100px; height: auto;">
-                    <p>Title: {{ album.title }}</p>
-                    <p>Author: {{ album.author }}</p>
-                    <p>Year: {{ album.year }}</p>
-                    <p>Genre: {{ album.genre }}</p>
-                </li>
-            </ul>
-    
+        <div class="wrapper d-flex justify-content-center flex-column  align-items-center">
+            <h1 class="text-center py-5">Popular albums</h1>
+            <!-- Utilizziamo il sistema di griglia di Bootstrap -->
+            <div class="row row-cols-1 row-cols-md-3 justify-content-center  container">
+                <div class="col" v-for="(album, index) in albumList" :key="album.title">
+                    <div class="card mb-4 box-shadow">
+                        <img :src="album.poster" class="card-img-top p-3" :alt="album.title">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ album.title }}</h5>
+                            <p class="card-text">Author: {{ album.author }}</p>
+                            <p class="card-text">Year: {{ album.year }}</p>
+                            <p class="card-text">Genre: {{ album.genre }}</p>        
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-  
 
     <!-- Includi Vue e Axios -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js"></script>
+    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
     <!-- Includi il file JavaScript -->
     <script src="./js/main.js"></script>
 </body>
